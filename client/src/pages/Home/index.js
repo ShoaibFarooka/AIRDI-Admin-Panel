cdf17a3747fc44c5249765a07fe0ca0551ef2248
Shoaib Farooka <shoaibfarooka@gmail.com>
2024-05-01 07:06:01 +0500

    Home route fix

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './index.css';
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-    return(
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        navigate('/ticket-validation');
+    }, [navigate]);
+
+    return (
         <div className="home">
             HOME page
         </div>
